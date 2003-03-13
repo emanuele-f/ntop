b8ab2cbbe9be1b003670eb59bdcc0b3e99e3e671
burton <burton@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2003-03-13 16:37:13 +0000

    Release roll to 2.1.58
    -----Burton
    
    
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntop@1545 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- ntop_win32.c
+++ ntop_win32.c
@@ -80,9 +80,9 @@ void initWinsock32() {
     exit(-1);
   }
 
-  version = "2.1.58pre";
+  version = "2.1.58";
   author  = "Luca Deri <deri@ntop.org>";
-  buildDate = "2003-02-xx";
+  buildDate = "2003-03-13";
 
   if(!isWinNT()) {
     osName = "Win95/98/ME";

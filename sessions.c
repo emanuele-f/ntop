22c647240dbff29d7c10b4c9aaccef15d8509683
deri <deri@a02cd0c1-8e76-42e1-a119-56f9641475e2>
2012-11-18 10:40:35 +0000

    Crash fix with white lists
    git-svn-id: https://svn.ntop.org/svn/ntop/trunk/ntop@5813 a02cd0c1-8e76-42e1-a119-56f9641475e2

--- sessions.c
+++ sessions.c
@@ -2604,13 +2604,15 @@ static IPSession* handleTCPUDPSession(u_int proto, const struct pcap_pkthdr *h,
 
   myGlobals.device[actualDeviceId].l7.protoTraffic[theSession->l7.major_proto] += h->len;
 
-  if(myGlobals.l7.numSupportedProtocols > theSession->l7.major_proto) {
-    srcHost->l7.traffic[theSession->l7.major_proto].bytesSent += h->len;
-    dstHost->l7.traffic[theSession->l7.major_proto].bytesRcvd += h->len;
-  } else
-    traceEvent(CONST_TRACE_WARNING, "Internal error: protocol overflow [%u/%u] (too many protocols)",
-	       theSession->l7.major_proto, myGlobals.l7.numSupportedProtocols);
-
+  if(srcHost->l7.traffic) {
+    if(myGlobals.l7.numSupportedProtocols > theSession->l7.major_proto) {
+      srcHost->l7.traffic[theSession->l7.major_proto].bytesSent += h->len;
+      dstHost->l7.traffic[theSession->l7.major_proto].bytesRcvd += h->len;
+    } else
+      traceEvent(CONST_TRACE_WARNING, "Internal error: protocol overflow [%u/%u] (too many protocols)",
+		 theSession->l7.major_proto, myGlobals.l7.numSupportedProtocols);
+  }
+  
   /* Immediately free the session */
   if(theSession->sessionState == FLAG_STATE_TIMEOUT) {
     if(myGlobals.device[actualDeviceId].sessions[idx] == theSession) {
